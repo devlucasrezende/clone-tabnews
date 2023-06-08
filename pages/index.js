203cb55565eed09ef8675f4fb6e2790d8755f744
devlucasrezende <83356058+devlucasrezende@users.noreply.github.com>
2023-06-08 22:14:11 +0000

    mensagem mãe

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Teste</h1>
+    return <h1>Oi mãe, tô aqui estudando programação (já vou dormir, não preocupa) mas queria só passar pra dizer que te amo!</h1>
 }
 
 export default Home;
